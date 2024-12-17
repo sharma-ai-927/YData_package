fb5548ae08ae88199c91e9656974f44679be0c10
Roy Lederman <lederman@users.noreply.github.com>
2024-12-17 14:25:41 -0500

    Updated to point to lederman user

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='YData',
-    version='0.1.0000',
+    version='0.1.0001',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A package to download YData files',
     # long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/emeyers/YData',
+    url='https://github.com/lederman/YData',
     author='Ethan Meyers',
     author_email='ethan.meyers@yale.edu'
 )
